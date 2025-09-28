72b17cd81f51b8f44fa602dd89fbcff82744c468
Tien Hwng <hungnguyentien12345@gmail.com>
2025-09-28 15:19:03 +0700

    add some minor changes

--- Lab 2/Core/Src/main.c
+++ Lab 2/Core/Src/main.c
@@ -44,7 +44,11 @@ TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN PV */
 
-uint8_t segmentMap[10] = {
+int counter = 50;
+
+int segToDisplay = 0;
+
+int segmentMap[10] = {
 	0b1111110, // 0
 	0b0110000, // 1
 	0b1101101, // 2
@@ -55,6 +59,14 @@ uint8_t segmentMap[10] = {
 	0b1110000, // 7
 	0b1111111, // 8
 	0b1111011  // 9
+};
+
+int SEG_Pins[7] = {
+	SEG0_Pin, SEG1_Pin, SEG2_Pin, SEG3_Pin, SEG4_Pin, SEG5_Pin, SEG6_Pin
+};
+
+int EN_Pins[2] = {
+	EN0_Pin, EN1_Pin
 };
 
 /* USER CODE END PV */
@@ -72,15 +84,12 @@ static void MX_TIM2_Init(void);
 
 void display7SEG(int num)
 {
-	uint8_t bitmask1 = segmentMap[num];
+	int bitmask = segmentMap[num];
 
-	HAL_GPIO_WritePin(GPIOB, SEG0_Pin, (bitmask1 & (1 << 6)) ? RESET : SET);
-	HAL_GPIO_WritePin(GPIOB, SEG1_Pin, (bitmask1 & (1 << 5)) ? RESET : SET);
-	HAL_GPIO_WritePin(GPIOB, SEG2_Pin, (bitmask1 & (1 << 4)) ? RESET : SET);
-	HAL_GPIO_WritePin(GPIOB, SEG3_Pin, (bitmask1 & (1 << 3)) ? RESET : SET);
-	HAL_GPIO_WritePin(GPIOB, SEG4_Pin, (bitmask1 & (1 << 2)) ? RESET : SET);
-	HAL_GPIO_WritePin(GPIOB, SEG5_Pin, (bitmask1 & (1 << 1)) ? RESET : SET);
-	HAL_GPIO_WritePin(GPIOB, SEG6_Pin, (bitmask1 & (1 << 0)) ? RESET : SET);
+	for (int i = 0; i <= 6; i++)
+	{
+		HAL_GPIO_WritePin(GPIOB, SEG_Pins[i], (bitmask & (1 << (6 - i))) ? RESET : SET);
+	}
 }
 
 /* USER CODE END 0 */
@@ -259,9 +268,6 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
-int counter = 50;
-int displayNum = 0;
-
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
 	counter--;
@@ -270,20 +276,22 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	{
 		counter = 50;
 
-		HAL_GPIO_WritePin(GPIOA, EN0_Pin, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOA, EN1_Pin, GPIO_PIN_SET);
+		for (int i = 0; i < 2; i++)
+		{
+			HAL_GPIO_WritePin(GPIOA, EN_Pins[i], GPIO_PIN_SET);
+		}
 
-		if(displayNum == 0){
-			HAL_GPIO_WritePin(GPIOA, EN0_Pin, GPIO_PIN_RESET);
+		if(segToDisplay == 0) {
+			HAL_GPIO_WritePin(GPIOA, EN_Pins[segToDisplay], GPIO_PIN_RESET);
 			display7SEG(1);
 		}
 
-		if(displayNum == 1){
-			HAL_GPIO_WritePin(GPIOA, EN1_Pin, GPIO_PIN_RESET);
+		if(segToDisplay == 1) {
+			HAL_GPIO_WritePin(GPIOA, EN_Pins[segToDisplay], GPIO_PIN_RESET);
 			display7SEG(2);
 		}
 
-		displayNum = (displayNum + 1) % 2;
+		segToDisplay = (segToDisplay + 1) % 2;
 	}
 }
 
